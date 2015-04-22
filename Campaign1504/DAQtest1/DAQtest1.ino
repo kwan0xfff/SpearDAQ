513f1ce1b1a39f85c2298b4e5b51b9f58d86861e
R. J. Dev. Kwan <kwan0xfff@gmail.com>
2015-04-21 18:00:57 -0700

    General code clean-up: pins, writes.
    * Ensure pin numbers are integers.
    * Avoid repetitive float divide (1.0/....); put in setup().
    * Merge some tab writes.
    * Remove commented out code.

--- Campaign1504/DAQtest1/DAQtest1.ino
+++ Campaign1504/DAQtest1/DAQtest1.ino
@@ -3,11 +3,11 @@
 
 unsigned long time;
 const int chipSelect = 4;
-float pressure = 1;
-float flowH = 2;
-float flowK = 3;
-float temp = 4;
-float psi = 0;
+int pressure = 1;
+int flowH = 2;
+int flowK = 3;
+int temp = 4;
+float psi = 0.0;
 float reading = 0;
 int x = 0;
 float H = 0;
@@ -18,8 +18,12 @@ float T = 0;
 float temperature = 0;
 File dataFile;
 
+float psicoeff;         // 1/1.6384
+
 void setup()
 {
+  psicoeff = 1.0/1.6384;
+
   pinMode(pressure, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(flowH, OUTPUT);
@@ -39,6 +43,10 @@ void setup()
   }
   Serial.println("Card initialized.");
   dataFile = SD.open("pressure.txt", O_CREAT | O_WRITE | O_APPEND);
+  while (!dataFile)
+  {
+    ;
+  }
 }
 
 void loop()
@@ -51,47 +59,26 @@ void loop()
 
   // reading: 921.6 @ 4.5V -> 500 psi
   // reading: 102.4 @ 0.5V ->   0 psi
-  psi = (1/1.6384)*(reading-102.4);
+  psi = psicoeff*(reading-102.4);
   kero = .0355073*K-.2924;
   hyd = .046377*H-.006668;
-  temperature = 1.2207*T;   
-  //File dataFile = SD.open("pressure.txt", O_CREAT | O_WRITE | O_APPEND);
-  while (!dataFile)
-  {
-    ;
-  }
+  temperature = 1.2207*T;
   if (dataFile)
   {
     dataFile.println();
     dataFile.print(time/1000.000);
-    dataFile.write('\t');
-    dataFile.write('\t');
+    dataFile.write('\t\t');
     dataFile.print(psi);
-    dataFile.write('\t');
-    dataFile.write('\t');
+    dataFile.write('\t\t');
     dataFile.print(kero);
-    dataFile.write('\t');
-    dataFile.write('\t');
+    dataFile.write('\t\t');
     dataFile.print(hyd);
-    dataFile.write('\t');
-    dataFile.write('\t');
+    dataFile.write('\t\t');
     dataFile.print(temperature);
-    //dataFile.close();
     dataFile.flush();
-//    Serial.write("Time: ");
-//    Serial.print(time);
-//    Serial.write("pressure: ");
-//    Serial.print(psi);
-//    Serial.print('\t');
-//    Serial.write("psi");
-//    Serial.print('\t');
-//    Serial.write("reading: ");
-//    Serial.print(reading);
-//    Serial.print('\n');
   }
   else
   {
      Serial.println("Error opening text file");
   }
-  //delay(50);
 }
